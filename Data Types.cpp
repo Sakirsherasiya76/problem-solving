a399e39516bfa3ed3990c1ed841fb86c7000a6e3
Sakir Sherasiya <113710372+Sakirsherasiya76@users.noreply.github.com>
2023-01-21 10:28:26 -0800

    Create Data Types.cpp

---
+++ Data Types.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a; long b; char c; float f; double d;
+    cin>>a>>b>>c>>f>>d;
+    cout<<a<<"\n"<<b<<"\n"<<c<<"\n";
+    cout<<fixed<<setprecision(3)<<f<<"\n";
+    cout<<fixed<<setprecision(9)<<d<<"\n";
+    // Complete the code.
+    return 0;
+}
